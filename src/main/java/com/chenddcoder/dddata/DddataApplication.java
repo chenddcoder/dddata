d6cd5baaf8c6075f9bbeabba89a4c257c5cf1e1c
chendingding <chendingding@huan.tv>
2020-12-24 10:45:45 +0800

    init

---
+++ src/main/java/com/chenddcoder/dddata/DddataApplication.java
@@ -0,0 +1,13 @@
+package com.chenddcoder.dddata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chenddcoder/dddata/DddataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenddcoder.dddata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
